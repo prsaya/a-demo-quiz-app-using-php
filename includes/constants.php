b00676dea031fd18875dbede3ed645976939336a
Prasad Saya <38682661+prsaya@users.noreply.github.com>
2023-01-07 11:19:51 +0530

    Update constants.php

--- includes/constants.php
+++ includes/constants.php
@@ -2,11 +2,7 @@
 
 	class Constants {
 		
-		//const JSON_DATA_FILE = "quiz_data_empty.txt";
-		//const JSON_DATA_FILE = "quiz_data_x.txt";
 		const JSON_DATA_FILE = "data/quiz_data.txt";
-		//const JSON_DATA_FILE = "none.txt";
-
 		const QUIZ_FORM = "quiz_form.php";
 		const QUIZ_RESULT = "quiz_result.php";
 		const QUIZ_START = "quiz_start.php";
