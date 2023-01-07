d9cd749dead6e71ca018777ea906d689d716934c
Prasad Saya <38682661+prsaya@users.noreply.github.com>
2023-01-07 11:25:50 +0530

    Update redirect_start.php

--- redirect_start.php
+++ redirect_start.php
@@ -3,10 +3,6 @@
 	/*
 	 * Redirects to the quiz starter page.
 	 */
-	//session_start();
-	//session_unset();
-	//session_destroy();
-	//("C:\php_includes\quiz_app_3");
 	require_once("includes/constants.php");
 	header("Location: " . Constants::QUIZ_START);
 	exit;
